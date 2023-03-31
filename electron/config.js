6127a282053c03efa2076400f7b1886aa72d7046
huangshengwen <2804964069@qq.com>
2023-03-31 14:44:54 +0800

    删除无用内容

--- electron/config.js
+++ electron/config.js
@@ -2,8 +2,8 @@ const path = require('path')
 
 const isCreateAdmin = true
 const isDev = true
-const UiUrl = isDev ? 'http://127.0.0.1:5173/' : path.join(__dirname,'../uidist/index.html')
-const AdminUrl = isCreateAdmin ? (isDev ? 'http://172.16.18.244:8081/' : path.join(__dirname,'../admindist/index.html')) : ''
+const UiUrl = isDev ? '' : path.join(__dirname,'../uidist/index.html')
+const AdminUrl = isCreateAdmin ? (isDev ? '' : path.join(__dirname,'../admindist/index.html')) : ''
 const openDevTools =  ['admin']//['ui','admin']
 module.exports = {
     isCreateAdmin,
